c91b575bcb8303ab9a1f4a0c206fe10c7fcc72bc
Lobivan <79607911231.il@gmail.com>
2024-08-25 17:22:20 +0700

    removed unused code

--- prog1/main.cpp
+++ prog1/main.cpp
@@ -1,31 +1,7 @@
-#include <iostream>
-#include <string>
-#include <vector>
-#include <ctype.h>
-#include <math.h>
 #include "prog1.hpp"
-
-#define INPUT_MAX_LEN 64
-using namespace std;
-
-
-void task3(string num) {
-    int n = num.length();
-    if (n < 3) {
-        cout << "Error: sum must be bigger than 99. Recieved: " << num << "\n";
-        return;
-    }
-    int last_five_digits = stoi(num.substr(max(0, n - 5)));
-
-    if (last_five_digits % 32 == 0)
-        cout << "Data recieved!\n";
-    else
-        cout << "Error: Recieved number is not divisible by 32!\n";
-}
 
 int main() {
     Prog1 p1;
     p1.run();
-
     return 0;
 }
